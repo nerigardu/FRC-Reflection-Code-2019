6300f5e83ee5f45a40b8ecb89bb1b91380bb4d03
Epicsamurott33 <32318538+Epicsamurott33@users.noreply.github.com>
2019-03-05 13:32:39 -0600

    Add files via upload

---
+++ ReflectionCode.java
@@ -0,0 +1,296 @@
+package frc.robot;
+
+import java.lang.Math;
+
+// This is very jank code but it should give an idea of what needs to be done. If it runs, great!
+
+public class Robot extends IterativeRobot
+{
+    
+    public void autonomousInit() // Needs to be some sort of initializing function
+    {
+        // Initialize the table
+        NetworkTable table;
+
+        // Setup the length of middles
+        double[] middles = new doubles[20]; // There will be errors if we exceed 20, but there's no way there will be 40 midpoints.
+    }
+
+    public void autonomousPeriodic() // Needs to be some sort of looping function
+    {
+        // Table should constantly update to get new values
+        table = NetworkTable.getTable("vision/boundingRect");
+
+        // In case the table isn't returning any values, this will return a default value
+        double[] defaultValue = new double[0];
+
+        // Set corresponding values from networkTables into program variables
+        double[] x = table.getNumberArray("center_x_positions", defaultValue);
+        double[] y = table.getNumberArray("center_y_positions", defaultValue);
+        double[] h = table.getNumberArray("height", defaultValue);
+        double[] w = table.getNumberArray("width", defaultValue);
+
+        /* Processing needs to be done in order to ensure we get the middle
+        of the middles of all the boundingRects. This will allow the robot
+        to center and orient itself towards the target. */
+
+        // If the length of x isn't 0, process the midpoints...
+        if(x.length != 0)
+        {
+            // Alter 'sub' for the future for loop so it runs properly
+            if((x.length % 2) == 0)
+            {
+                // If even
+                int sub = 0;
+            }
+
+            else
+            {
+                // Else odd
+                int sub = -1;
+            }
+
+            int inc = 0; // Just to make sure the counting works properly in the for loop
+            
+            // Run a for loop that will produce the midpoints of 2 points (Trust me it makes sense)
+            for(int i=0;(i<=(x.length + sub)); i++)
+            {
+                // We need to find the middle of the mid-points of each set of 2
+                
+                // First, make sure we are on an odd increment
+                if((i % 2) != 0)
+                {
+                    // If we are on an odd increment, we find the midpoint of a specific set of 2 points 
+                    middles[inc++] = (x[i-1] + x[i]) / 2;
+                }
+
+            }
+
+            /* From here, middles[] has the midpoints of every pair of midpoints that can be found. [0], [1]
+            has been averaged, [2], [3] has been averaged, as so on, depending on the value of x.length. Even
+            and odd statements are to make sure that only pairs will be able to be averaged, and that extra
+            values will be excluded.
+
+            Now that we have the midpoints, the next challenge is to make the robot choose the close of these
+            points, and afterwards has to move itself closer so the midpoint is centered in the camera's vision. */
+
+            double left = -1;
+            double right = 1;
+            double forward = 1;
+            double none = 0;
+            
+            // Super lazy if-statement that will fail if there are somehow more than 6 mid-midpoints
+            if(inc >= 1)
+            {
+                if(inc == 1)
+                {
+                    int num = 0;
+                }
+                
+                if(inc == 2)
+                {
+                    int a = Math.abs(320 - middles[0]);
+                    int b = Math.abs(320 - middles[1]);
+
+                    if(a < b)
+                    { 
+                        int num = 0;
+                    }
+
+                    if(b < a)
+                    {
+                        int num = 1;
+                    }
+
+                }
+
+                if(inc == 3)
+                {
+                    int a = Math.abs(320 - middles[0]);
+                    int b = Math.abs(320 - middles[1]);
+                    int c = Math.abs(320 - middles[2]);
+
+                    if(a < b && a < c)
+                    {
+                        int num = 0;
+                    }
+
+                    if(b < a && b < c)
+                    {
+                        int num = 1;
+                    }
+
+                    if(c < a && c < b)
+                    {
+                        int num = 2;
+                    }
+
+                }
+
+                if(inc == 4)
+                {
+                    int a = Math.abs(320 - middles[0]);
+                    int b = Math.abs(320 - middles[1]);
+                    int c = Math.abs(320 - middles[2]);
+                    int d = Math.abs(320 - middles[3]);
+
+                    if(a < b && a < c && a < d)
+                    {
+                        int num = 0;
+                    }
+
+                    if(b < a && b < c && b < d)
+                    {
+                        int num = 1;
+                    }
+
+                    if(c < a && c < b && c < d)
+                    {
+                        int num = 2;
+                    }
+
+                    if(d < a && d < b && d < c)
+                    {
+                        int num = 3;
+                    }
+
+                }
+
+                if(inc == 5)
+                {
+                    int a = Math.abs(320 - middles[0]);
+                    int b = Math.abs(320 - middles[1]);
+                    int c = Math.abs(320 - middles[2]);
+                    int d = Math.abs(320 - middles[3]);
+                    int e = Math.abs(320 - middles[4]);
+
+                    if(a < b && a < c && a < d && a < e)
+                    {
+                        int num = 0;
+                    }
+
+                    if(b < a && b < c && b < d && b < e)
+                    {
+                        int num = 1;
+                    }
+
+                    if(c < a && c < b && c < d && c < e)
+                    {
+                        int num = 2;
+                    }
+
+                    if(d < a && d < b && d < c && d < e)
+                    {
+                        int num = 3;
+                    }
+
+                    if(e < a && e < b && e < c && e < d)
+                    {
+                        int num = 4;
+                    }
+
+                }
+
+                if(inc == 6)
+                {
+                    int a = Math.abs(320 - middles[0]);
+                    int b = Math.abs(320 - middles[1]);
+                    int c = Math.abs(320 - middles[2]);
+                    int d = Math.abs(320 - middles[3]);
+                    int e = Math.abs(320 - middles[4]);
+                    int f = Math.abs(320 - middles[5]);
+
+                    if(a < b && a < c && a < d && a < e && a < f)
+                    {
+                        int num = 0;
+                    }
+
+                    if(b < a && b < c && b < d && b < e && b < f)
+                    {
+                        int num = 1;
+                    }
+
+                    if(c < a && c < b && c < d && c < e && c < f)
+                    {
+                        int num = 2;
+                    }
+
+                    if(d < a && d < b && d < c && d < e && d < f)
+                    {
+                        int num = 3;
+                    }
+
+                    if(e < a && e < b && e < c && e < d && e < f)
+                    {
+                        int num = 4;
+                    }
+
+                    if(f < a && f < b && f < c && f < d && f < e)
+                    {
+                        int num = 5;
+                    }
+
+                }
+
+                // Make the robot move!
+                if(middles[num] > 322)
+                {
+                    mecanumDrive_Cartesian(left, none, none);
+                }
+
+                if(middles[num] < 318)
+                {
+                    mecanumDrive_Cartesian(right, none, none);
+                }
+
+                if(middles[0] < 322 && middles[1] > 318)
+                {
+                    mecanumDrive_Cartesian(none, forward, none);
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+/*
+package frc.robot;
+
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.SampleRobot;
+
+public class Robot extends SampleRobot
+{
+    
+    NetworkTable table;
+
+    public Robot()
+    {
+        table = NetworkTable.getTable("vision/boundingRect");
+    }
+
+    public void robotInit()
+    {
+        double[] defaultValue = new double[0];
+
+        while (true)
+        {
+            double[] centers = table.getNumberArray("centers", defaultValue);
+            System.out.print("centers: ");
+
+            for (double center : centers)
+            {
+                System.out.print(center + " ");
+            }
+
+            Ststem.out.println();
+            Timer.delay(1);
+        }
+
+    }
+
+}
+*/
---
+++ VisionCodeGUI+LocalTable.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python3
+
+from grip import GripPipeline # Note: The contour filter is based off of a 30x30 square
+from networktables import NetworkTables
+import cv2
+import numpy
+import os
+
+# Note: Coordinates of BoundingRect are based off of Quadrant IV (+ Bottom right)
+
+# Change booleans to enable or disable cetain functions (Note that corners will not be published if corresponding processing is False)
+printValues = True
+usingBoundingRect = True
+usingMinAreaRect = False
+publishingCornersMin = False
+enableGUI = True
+
+# Video device assignment (Change accordingly) (NEED TO CHANGE OS COMMAND BELOW AS WELL):
+videoDevice = 1
+
+# NetworkTables server assignment (Change accordingly):
+server = '127.0.0.1'
+
+def publishData(pipeline):
+    
+    if usingBoundingRect:
+        boundingRect_FULL = []
+        center_x_positions = []
+        center_y_positions = []
+        widths = []
+        heights = []
+
+    if usingMinAreaRect:
+        minAreaRect_FULL = []
+        rect_center = []
+        width_height = []
+        angle_of_rotation = 0
+
+    if publishingCornersMin:
+        MIN_corner_a = []
+        MIN_corner_b = []
+        MIN_corner_c = []
+        MIN_corner_d = []
+
+    # Find the bounding rectangles of the contours to get variables for usage in robot code
+    for contour in pipeline.filter_contours_output:
+        
+        if usingBoundingRect:
+            boundingRect_FULL = cv2.boundingRect(contour) # Data mashed into one variable
+            x, y, w, h = cv2.boundingRect(contour) # Data spread out over 4 variables
+
+            # (Technically) Extra processing for boundingRect variables
+            center_x_positions.append(x + w / 2)  # X and Y are coordinates of the top-left corner of the bounding box
+            center_y_positions.append(y + h / 2)
+            widths.append(w)
+            heights.append(h)
+        
+        if usingMinAreaRect:
+            minAreaRect_FULL = cv2.minAreaRect(contour) # Data mashed into one variable
+            rect_center, width_height, angle_of_rotation = cv2.minAreaRect(contour) # Data spread out over 3 variables
+
+            if publishingCornersMin:
+                MIN_corner_a, MIN_corner_b, MIN_corner_c, MIN_corner_d = cv2.boxPoints(minAreaRect_FULL)
+
+    # Publish to NetworkTables and print out data (if enabled):
+    if usingBoundingRect:
+        tableBound = NetworkTables.getTable('/vision/boundingRect')
+        tableBound.putNumberArray('center_x_positions', center_x_positions)
+        tableBound.putNumberArray('center_y_positions', center_y_positions)
+        tableBound.putNumberArray('width', widths)
+        tableBound.putNumberArray('height', heights)
+
+        if printValues:
+            print('---boundingRect Data---')
+            print('boundingRect_FULL:')
+            print(boundingRect_FULL)
+            print('center_x_positions:')
+            print(center_x_positions)
+            print('center_y_positions:')
+            print(center_y_positions)
+            print('widths:')
+            print(widths)
+            print('heights:')
+            print(heights)
+
+            print('\n\n')
+    
+    if usingMinAreaRect:
+        tableMin = NetworkTables.getTable('/vision/minAreaRect')
+        tableMin.putNumberArray('rect_center', rect_center)
+        tableMin.putNumberArray('width_height', width_height)
+        tableMin.putNumber('angle_of_rotation', angle_of_rotation)
+
+        if printValues:
+            print('---minAreaRect Data---')
+            print('minAreaRect_FULL:')
+            print(minAreaRect_FULL)
+            print('rect_center:')
+            print(rect_center)
+            print('width_height:')
+            print(width_height)
+            print('angle_of_rotation:')
+            print(angle_of_rotation)
+
+        if publishingCornersMin:
+            tableMinCorners = NetworkTables.getTable('/vision/minAreaRect/corners')
+            tableMinCorners.putNumberArray('MIN_corner_a', MIN_corner_a)
+            tableMinCorners.putNumberArray('MIN_corner_b', MIN_corner_b)
+            tableMinCorners.putNumberArray('MIN_corner_c', MIN_corner_c)
+            tableMinCorners.putNumberArray('MIN_corner_d', MIN_corner_d)
+
+            if printValues:
+                print('MIN_corner_a:')
+                print(MIN_corner_a)
+                print('MIN_corner_b:')
+                print(MIN_corner_b)
+                print('MIN_corner_c:')
+                print(MIN_corner_c)
+                print('MIN_corner_d:')
+                print(MIN_corner_d)
+
+        if printValues:
+            print('\n\n')
+
+
+def main():
+    print('Initializing NetworkTables...')
+    NetworkTables.initialize(server=server)
+
+    print('Creating Video Capture...')
+    cap = cv2.VideoCapture(videoDevice)
+
+    # Change Exposure (***change video device as needed***):
+    os.system('v4l2-ctl -d /dev/video1 -c exposure_auto=1 -c exposure_absolute=0')
+
+    print('Creating Pipeline...')
+    pipeline = GripPipeline()
+    
+    print('Running Pipeline.')
+    while cap.isOpened():
+        have_frame, frame = cap.read()
+
+        if have_frame:
+            pipeline.process(frame)
+            publishData(pipeline)
+
+        if enableGUI:
+            # Set thickness of lines
+            thickness = 2
+
+            # Draw the normal contours (Green):
+            cv2.drawContours(frame, pipeline.filter_contours_output, -1, (0,255,0), thickness)
+
+            # Draw the proper rectangles:
+            for contour in pipeline.filter_contours_output:
+                
+                # Minimum Bounding Rectangle:
+                minAreaRect_FULL = cv2.minAreaRect(contour)
+                boxCorners = cv2.boxPoints(minAreaRect_FULL)
+                
+                # Numpy Conversion:
+                box = numpy.array(boxCorners).reshape((-1,1,2)).astype(numpy.int32)
+                
+                # Draw rotated rectangle (Red):
+                cv2.drawContours(frame, [box], -1, (0,0,255), thickness)
+
+                # Normal Bounding Rectangle:
+                x,y,w,h = cv2.boundingRect(contour)
+                cv2.rectangle(frame,(x,y),(x+w,y+h), (255,0,0), thickness)
+
+            # Show all data in a window:
+            cv2.imshow('FRC Vision', frame)
+            
+            # Kill if 'q' is pressed:
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+    print('Capture closed.')
+
+
+if __name__ == '__main__':
+    main()
